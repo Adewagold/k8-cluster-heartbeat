8ee2636552b603d187e35c9eef1f93581a7f6b36
Adewale Adeleye <Adewagold@yahoo.com>
2020-08-29 11:04:51 +0100

    Add app create endpoing

---
+++ src/main/java/com/delinktech/heartbeat/appheartbeat/server/config/AppConfig.java
@@ -0,0 +1,7 @@
+package com.delinktech.heartbeat.appheartbeat.server.config;
+
+/**
+ * *  Created by Adewale Adeleye on 2020-08-25
+ **/
+public class AppConfig {
+}
---
+++ src/main/java/com/delinktech/heartbeat/appheartbeat/server/controller/AppController.java
@@ -0,0 +1,7 @@
+package com.delinktech.heartbeat.appheartbeat.server.controller;
+
+/**
+ * *  Created by Adewale Adeleye on 2020-08-29
+ **/
+public class AppController {
+}
